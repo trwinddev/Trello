6615f35666d8af4b2972c1a57b3376a8f0275d40
phongngotrong <ngotrongphong.dev@gmail.com>
2023-01-07 15:27:04 +0700

    design trello front-end

--- src/App.js
+++ src/App.js
@@ -1,7 +1,102 @@
-import "./App.css";
+import "./App.scss";
 
 function App() {
-  return <div className="App">test</div>;
+  return (
+    <div className="App">
+      <nav className="navbar app">App bar</nav>
+      <nav className="navbar board">Board bar</nav>
+      <nav className="board-columns">
+        <div className="column">
+          <header>Phenikaa U</header>
+          <ul>
+            <li>
+              <img
+                src="https://images.unsplash.com/photo-1672942935544-588b4b9ea1a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+                alt=""
+              />
+            </li>
+            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </li>
+            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </li>
+            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </li>
+            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </li>
+            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </li>
+            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </li>
+            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </li>
+            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </li>
+            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </li>
+            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </li>
+            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </li>
+            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Phenikaa U</header>
+          <ul>
+            <li>
+              <img
+                src="https://images.unsplash.com/photo-1672942935544-588b4b9ea1a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+                alt=""
+              />
+            </li>
+            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </li>
+            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </li>
+            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </li>
+            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Phenikaa U</header>
+          <ul>
+            <li>
+              <img
+                src="https://images.unsplash.com/photo-1672942935544-588b4b9ea1a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+                alt=""
+              />
+            </li>
+            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </li>
+            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </li>
+            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </li>
+            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Phenikaa U</header>
+          <ul>
+            <li>
+              <img
+                src="https://images.unsplash.com/photo-1672942935544-588b4b9ea1a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+                alt=""
+              />
+            </li>
+            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </li>
+            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </li>
+            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </li>
+            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Phenikaa U</header>
+          <ul>
+            <li>
+              <img
+                src="https://images.unsplash.com/photo-1672942935544-588b4b9ea1a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+                alt=""
+              />
+            </li>
+            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </li>
+            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </li>
+            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </li>
+            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+      </nav>
+    </div>
+  );
 }
 
 export default App;
